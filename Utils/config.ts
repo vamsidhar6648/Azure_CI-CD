c35e8cee2fec35d99f08257be095276b4ee39cec
vamsidhar6648 <vamsii2486@gmail.com>
2025-06-09 18:31:09 +0530

    config added

---
+++ Utils/config.ts
@@ -0,0 +1,5 @@
+export const baseURL="https://hr.quality-matrix.us/web/index.php/auth/login";
+export const userName="adminnine";
+export const password="Adminnine@123";
+export const screenshotfilepath="./screenshots/";
+export const filepath="./test-data/passwords.xlsx";
